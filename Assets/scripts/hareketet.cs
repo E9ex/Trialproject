4fd9b189536c6db1306354aecdb43b3c570699fb
Ege Can Sarı <89903168+E9ex@users.noreply.github.com>
2022-09-04 19:59:23 +0300

    abibibakarmısıntakıldımkaldim

--- Assets/scripts/hareketet.cs
+++ Assets/scripts/hareketet.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class hareketet : MonoBehaviour
 {
+    public Vector3 ilkros,ilkross,sonross ,sonros;
     public int puan = 0;
     public Text puangoster;
     public Transform top;
@@ -41,13 +42,49 @@ public class hareketet : MonoBehaviour
         {
             transform.Rotate(0,-1*Time.maximumParticleDeltaTime,0);
         }
-        if (Input.GetKey(KeyCode.O))
+
+
+        if (Input.GetMouseButton(1))
         {
-            transform.Rotate(0,0,1*Time.maximumParticleDeltaTime);
+
+
+            ilkross = Input.mousePosition;
         }
-        if (Input.GetKey(KeyCode.L))
+        else if (Input.GetMouseButton(1))
+            {
+                transform.Rotate(0,0,1*Time.maximumParticleDeltaTime);
+                float zrotation = Mathf.Clamp(transform.rotation.z,-5.52f, 5.52f);
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.eulerAngles.y, zrotation);
+                sonrosa = Input.mousePosition;
+                float farkkk = sonrosa - ilkross;
+                transform.rotation(0, 0, farkk * Time.deltaTime);
+                
+            }
+        
+        
+           
+            
+
+        
+
+        if (Input.GetMouseButton(0))
         {
+            ilkros = Input.mousePosition;
+        }
+
+      else  if (Input.GetMouseButton(0))
+        {
+            
             transform.Rotate(0,0,-1*Time.maximumParticleDeltaTime);
+            float zzrotation = Mathf.Clamp(transform.rotation.z,-5.52f, 5.52f);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, zzrotation);
+            sonros = Input.mousePosition;
+            float farkk = sonros - ilkros;
+            transform.rotation(0, 0, farkk * Time.deltaTime);
+        }
+        
+            
+
         }
         
         
